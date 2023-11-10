a87dc7b8a713af9a4ec68f4c0a474d3aeb213ce7
theuntitled95 <theuntitled95@gmail.com>
2023-11-10 22:13:58 +0400

    project done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary
+        "light-red": "hsl(0, 100%, 67%)",
+        "orangey-yellow": "hsl(39, 100%, 56%)",
+        "green-teal": "hsl(166, 100%, 37%)",
+        "cobalt-blue": "hsl(234, 85%, 45%)",
+
+        // Gradients
+        "light-slate-blue": "hsl(252, 100%, 67%)",
+        "light-royal-blue": "hsl(241, 81%, 54%)",
+        "violet-blue": "hsla(256, 72%, 46%, 1)",
+        "persian-blue": "hsla(241, 72%, 46%, 0)",
+
+        // Neutral
+        "pale-blue": "hsl(221, 100%, 96%)",
+        "light-lavender": "hsl(241, 100%, 89%)",
+        "dark-gray-blue": "hsl(224, 30%, 27%)",
+      },
+    },
+    screens: {
+      mobile: "375px",
+      desktop: "1440px",
+    },
   },
   plugins: [],
 };
