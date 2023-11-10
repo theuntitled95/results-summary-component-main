01d945676c25b5862ab0af401894b38dc51b2140
theuntitled95 <theuntitled95@gmail.com>
2023-11-10 23:22:13 +0400

    project complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        // Primary
-        "light-red": "hsl(0, 100%, 67%)",
-        "orangey-yellow": "hsl(39, 100%, 56%)",
-        "green-teal": "hsl(166, 100%, 37%)",
-        "cobalt-blue": "hsl(234, 85%, 45%)",
-
-        // Gradients
-        "light-slate-blue": "hsl(252, 100%, 67%)",
-        "light-royal-blue": "hsl(241, 81%, 54%)",
-        "violet-blue": "hsla(256, 72%, 46%, 1)",
-        "persian-blue": "hsla(241, 72%, 46%, 0)",
-
-        // Neutral
-        "pale-blue": "hsl(221, 100%, 96%)",
-        "light-lavender": "hsl(241, 100%, 89%)",
-        "dark-gray-blue": "hsl(224, 30%, 27%)",
-      },
-    },
+    extend: {},
     screens: {
       mobile: "375px",
       desktop: "1440px",
+    },
+    fontFamily: {
+      sans: ["HankenGrotesk-Medium"],
+      bold: ["HankenGrotesk-Bold"],
+      extrabold: ["HankenGrotesk-ExtraBold"],
+    },
+    colors: {
+      // Primary
+      "light-red": "hsl(0, 100%, 67%)",
+      "orangey-yellow": "hsl(39, 100%, 56%)",
+      "green-teal": "hsl(166, 100%, 37%)",
+      "cobalt-blue": "hsl(234, 85%, 45%)",
+
+      // Gradients
+      "light-slate-blue": "hsl(252, 100%, 67%)",
+      "light-royal-blue": "hsl(241, 81%, 54%)",
+      "violet-blue": "hsla(256, 72%, 46%, 1)",
+      "persian-blue": "hsla(241, 72%, 46%, 0)",
+
+      // Neutral
+      "pale-blue": "hsl(221, 100%, 96%)",
+      "light-lavender": "hsl(241, 100%, 89%)",
+      "dark-gray-blue": "hsl(224, 30%, 27%)",
+      white: "#ffffff",
     },
   },
   plugins: [],
